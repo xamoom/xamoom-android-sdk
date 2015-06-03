925dfe69d7397fb783466ce207bbbc157c838087
Raphael Seher <raphael@xamoom.com>
2015-06-03 15:46:27 +0200

    Added loadingSpinner

--- app/src/main/java/com/xamoom/xamoom_android_sdk/xamoom_android_sdk/MainActivity.java
+++ app/src/main/java/com/xamoom/xamoom_android_sdk/xamoom_android_sdk/MainActivity.java
@@ -1,11 +1,13 @@
 package com.xamoom.xamoom_android_sdk.xamoom_android_sdk;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.xamoom.xamoom_android_sdk.xamoom_android_sdk.api.APICallback;
@@ -23,6 +25,7 @@ import com.xamoom.xamoom_android_sdk.xamoom_android_sdk.api.mapping.SpotMap;
 public class MainActivity extends ActionBarActivity  {
 
     private TextView outputTextView;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,121 +34,83 @@ public class MainActivity extends ActionBarActivity  {
 
         outputTextView = (TextView) findViewById(R.id.outputTextView);
 
-        /*
-        //test every api call
-        XamoomEndUserApi.getInstance().getContentById("a3911e54085c427d95e1243844bd6aa3", false, false, "de", new APICallback<ContentById>() {
-            @Override
-            public void finished(ContentById result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-
-                for (ContentBlock cb : result.getContent().getContentBlocks()) {
-                    Log.v("XamoomEndUserApi", "Class: + " + cb);
-                }
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getContentbyIdFull("a3911e54085c427d95e1243844bd6aa3", false, false, "de", true, new APICallback<ContentById>() {
-            @Override
-            public void finished(ContentById result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getContentByLocationIdentifier("0ana0", false, false, "de", new APICallback<ContentByLocationIdentifier>() {
-            @Override
-            public void finished(ContentByLocationIdentifier result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getContentByLocation(46.615119, 14.262106, "de", new APICallback<ContentByLocation>() {
-            @Override
-            public void finished(ContentByLocation result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getSpotMap("0", new String[]{"stw"}, "de", new APICallback<SpotMap>() {
-            @Override
-            public void finished(SpotMap result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getClosestSpots(46.615119, 14.262106, "de", 100, 10, new APICallback<SpotMap>() {
-            @Override
-            public void finished(SpotMap result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-
-        XamoomEndUserApi.getInstance().getContentList("de", 7, null, new String[]{"artists"}, new APICallback<ContentList>() {
-            @Override
-            public void finished(ContentList result) {
-                Log.v("XamoomEndUserApi", "Worked! " + result);
-            }
-        });
-        */
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setIndeterminate(false);
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
     }
 
     public void getByIdButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getContentById("a3911e54085c427d95e1243844bd6aa3", false, false, "de", new APICallback<ContentById>() {
             @Override
             public void finished(ContentById result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getByIdFullButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getContentbyIdFull("a3911e54085c427d95e1243844bd6aa3", false, false, "de", true, new APICallback<ContentById>() {
             @Override
             public void finished(ContentById result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getByLocationIdentifierButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getContentByLocationIdentifier("0ana0", false, false, "de", new APICallback<ContentByLocationIdentifier>() {
             @Override
             public void finished(ContentByLocationIdentifier result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getByLocationFullButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getContentByLocation(46.615119, 14.262106, "de", new APICallback<ContentByLocation>() {
             @Override
             public void finished(ContentByLocation result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getSpotMapButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getSpotMap("0", new String[]{"stw"}, "de", new APICallback<SpotMap>() {
             @Override
             public void finished(SpotMap result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getClosestSpotsButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getClosestSpots(46.615119, 14.262106, "de", 100, 10, new APICallback<SpotMap>() {
             @Override
             public void finished(SpotMap result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
     }
 
     public void getContentListButtonOnClick (View v) {
+        mProgressDialog.show();
         XamoomEndUserApi.getInstance().getContentList("de", 7, null, new String[]{"artists"}, new APICallback<ContentList>() {
             @Override
             public void finished(ContentList result) {
+                mProgressDialog.dismiss();
                 outputTextView.setText("Output: " + result);
             }
         });
