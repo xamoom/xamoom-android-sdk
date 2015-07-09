1a0ba9a95cc5ff3e23ded457961be5f92077505c
Raphael Seher <raphael@xamoom.com>
2015-07-09 16:26:47 +0200

    Killed a lot

--- xamoom-android-sdk-app/xamoomcontentblocks/src/main/java/org/kobjects/htmlview/HasMeasuredPosition.java
+++
@@ -1,9 +0,0 @@
-package org.kobjects.htmlview;
-
-public interface HasMeasuredPosition {
-  int getMeasuredX();
-  int getMeasuredY();
-  void setMeasuredPosition(int x, int y);
-  void setMeasuredX(int x);
-  void setMeasuredY(int y);
-}
