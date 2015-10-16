b5a25edc14e58d03771ead77f20fea77c93e4194
Raphael Seher <raphael@xamoom.com>
2015-10-16 11:55:02 +0200

    Add comments and code cleaning
    Added javadoc comments to the XamoomBeaconService.

--- xamoom-android-sdk-app/xamoom-android-sdk/src/main/java/com/xamoom/android/XamoomBeaconService.java
+++ xamoom-android-sdk-app/xamoom-android-sdk/src/main/java/com/xamoom/android/XamoomBeaconService.java
@@ -23,10 +23,34 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 /**
- * XamoomBeaconService is a simple to use beaconService on top of android-beacon-library.
+ * XamoomBeaconService is a simple to use beaconService on top of the
+ * android-beacon-library. (https://github.com/AltBeacon/android-beacon-library)
  *
+ * To get started create a new Java class and extend Application.
+ * In the Manifest of your app add the new class.
+ *
+ * To get the beacon monitoring (incl. background-monitoring) started in the onCreate:
+ *<pre>
+ * {@code
+ * XamoomBeaconService.getInstance(getApplicationContext()).startBeaconService(MAJOR_ID);
+ * }
+ * </pre>
+ *
+ * The MAJOR_ID is the major-identifier you get from your xamoom system.
+ *
+ * You are also able to set automaticRanging and approximateDistanceRanging.
+ *
+ * AutomaticRanging means, that after entering a region it will automatically start ranging.
+ * (Will be wasted energy, if the user don't need this information)
+ *
+ * Changing approximateDistanceRanging will send you the IMMEDIATE_BEACON_BROADCAST,
+ * NEAR_BEACON_BROADCAST and FAR_BEACON_BROADCAST broadcasts.
+ *
+ * When manually starting ranging or monitoring, wait for the BEACON_SERVICE_CONNECT_BROADCAST
+ * broadcast.
  */
 public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, BeaconConsumer {
+
     private static final String TAG = "XamoomBeaconService";
 
     public static final String ENTER_REGION_BROADCAST = "com.xamoom.android.ENTER_REGION";
@@ -37,7 +61,8 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
     public static final String NEAR_BEACON_BROADCAST = "com.xamoom.android.NEAR_BEACON";
     public static final String FAR_BEACON_BROADCAST = "com.xamoom.android.FAR_BEACON";
     public static final String BEACONS = "com.xamoom.android.BEACONS";
-    public static final String BEACON_SERVICE_CONNECT_BROADCAST = "com.xamoom.android.BEACON_SERVICE_CONNECT_BROADCAST";
+    public static final String BEACON_SERVICE_CONNECT_BROADCAST =
+            "com.xamoom.android.BEACON_SERVICE_CONNECT_BROADCAST";
 
     private static XamoomBeaconService mInstance;
 
@@ -55,6 +80,12 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
     public boolean approximateDistanceRanging = false;
     public boolean fastInsideRegionScanning = true;
 
+    /**
+     * Method to get the singleton on XamoomBeaconService.
+     *
+     * @param context A context.
+     * @return An instance of XamoomBeaconService.
+     */
     public static XamoomBeaconService getInstance(Context context) {
         if (mInstance == null) {
             mInstance = new XamoomBeaconService();
@@ -64,13 +95,21 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         return mInstance;
     }
 
+    /**
+     * Start the XamoomBeaconService with a beacon majorId.
+     * This will automatically start (background-)monitoring for xamoom beacons
+     * with the used majorId.
+     *
+     * @param majorId MajorId you get from your xamoom system.
+     */
     public void startBeaconService(@NonNull String majorId) {
         Log.i(TAG, "startBeaconService");
 
         if (majorId.equalsIgnoreCase("")) {
             throw new IllegalArgumentException("MajorId should not be a number.");
         }
-        mRegion = new Region("test", Identifier.parse("de2b94ae-ed98-11e4-3432-78616d6f6f6d"), Identifier.parse(majorId), null);
+        mRegion = new Region("test", Identifier.parse("de2b94ae-ed98-11e4-3432-78616d6f6f6d"),
+                Identifier.parse(majorId), null);
 
         mRegionBootstrap = new RegionBootstrap(this, mRegion);
 
@@ -84,6 +123,13 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         mBeaconManager.bind(this);
     }
 
+    /**
+     * Change the backgroundScanningSpeed from the beaconManager. Can make your app use more
+     * energy than needed.
+     *
+     * @param betweenScanPeriod Value in ms to wait between scans.
+     * @param scanPeriod Value in ms to scan for beacons. Should not be below 1100 ms.
+     */
     private void setBackgroundScanningSpeeds(int betweenScanPeriod, int scanPeriod){
         mBeaconManager.setBackgroundBetweenScanPeriod(betweenScanPeriod);
         mBeaconManager.setBackgroundScanPeriod(scanPeriod);
@@ -95,6 +141,13 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /**
+     * Change the foregroundScanningSpeed from the beaconManager. Can make your app use more
+     * energy than needed.
+     *
+     * @param betweenScanPeriod Value in ms to wait between scans.
+     * @param scanPeriod Value in ms to scan for beacons. Should not be below 1100 ms.
+     */
     private void setForegroundScanningSpeeds(int betweenScanPeriod, int scanPeriod){
         mBeaconManager.setForegroundBetweenScanPeriod(betweenScanPeriod);
         mBeaconManager.setForegroundScanPeriod(scanPeriod);
@@ -106,6 +159,12 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /*
+     * Sends a broadcast with a broadcastId and beacons.
+     *
+     * @param broadcastId BroadcastId from XamoomBeaconService.
+     * @param beacons Beacons, if there should be beacons in the broadcast.
+     */
     private void sendBroadcast(String broadcastId, @Nullable ArrayList<Beacon> beacons) {
         Intent intent = new Intent(broadcastId);
 
@@ -116,6 +175,13 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         mContext.sendBroadcast(intent);
     }
 
+    /*
+     * Will be called when entering the region.
+     *
+     * Sends a ENTER_REGION_BROADCAST.
+     *
+     * @param region Monitored region.
+     */
     @Override
     public void didEnterRegion(Region region) {
         Log.i(TAG, "didEnterRegion");
@@ -131,6 +197,13 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /*
+     * Will be called when leaving a region.
+     *
+     * Sends a EXIT_REGION_BROADCAST.
+     *
+     * @param region Monitored region.
+     */
     @Override
     public void didExitRegion(Region region) {
         Log.i(TAG, "didExitRegion");
@@ -146,10 +219,24 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         sendBroadcast(EXIT_REGION_BROADCAST, null);
     }
 
+    /*
+     * Will be called, the monitoring state for a region changes.
+     */
     @Override
     public void didDetermineStateForRegion(int i, Region region) {
     }
 
+    /*
+     * Will be called when ranging beacons.
+     *
+     * Sends a FOUND_BEACON_BROADCAST.
+     *
+     * If you have enabled approximateDistanceRanging it will also send
+     * IMMEDIATE_BEACON_BROADCAST, NEAR_BEACON_BROADCAST and FAR_BEACON_BROADCAST broadcasts.
+     *
+     * @param beacons Beacons found in region.
+     * @param region Ranged region.
+     */
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
         Log.i(TAG, "didRangeBeaconsInRegion: " + beacons.size());
@@ -184,6 +271,9 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /**
+     * Start the ranging for beacons.
+     */
     public void startRangingBeacons() {
         Log.i(TAG, "Start ranging beacons");
 
@@ -194,6 +284,9 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /**
+     * Stop the ranging for beacons.
+     */
     public void stopRangingBeacons() {
         Log.i(TAG, "Stop ranging beacons");
 
@@ -204,16 +297,27 @@ public class XamoomBeaconService implements BootstrapNotifier, RangeNotifier, Be
         }
     }
 
+    /**
+     * Stop the monitoring for beacons. (Also stops background monitoring)
+     */
     public void stopMonitoringRegion() {
         Log.i(TAG, "Stop monitoring beacons");
         mRegionBootstrap.disable();
     }
 
+    /**
+     * Start the monitoring for beacons. (Also starts the background monitoring)
+     */
     public void startMonitoringRegion() {
         Log.i(TAG, "Start monitoring beacons");
         mRegionBootstrap = new RegionBootstrap(this, mRegion);
     }
 
+    /**
+     * Will be called, when the beaconManager is ready to be used.
+     *
+     * Sends a BEACON_SERVICE_CONNECT_BROADCAST.
+     */
     @Override
     public void onBeaconServiceConnect() {
         Log.i(TAG, "onBeaconServiceConnect");
