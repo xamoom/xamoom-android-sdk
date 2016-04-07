3323dc529945d2a526ac5a8b6a21d6f66e961ddb
xamoom-raphael <raphael@xamoom.com>
2016-04-07 20:05:51 +0200

    Change html video loading from webview to Thumbnail + Intent

--- xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/ViewHolders/ContentBlock2ViewHolder.java
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/ViewHolders/ContentBlock2ViewHolder.java
@@ -1,8 +1,14 @@
 package com.xamoom.android.xamoomcontentblocks.ViewHolders;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.media.MediaMetadataRetriever;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,13 +19,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubeIntents;
 import com.google.android.youtube.player.YouTubeThumbnailLoader;
 import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.squareup.picasso.Picasso;
 import com.xamoom.android.xamoomsdk.R;
 import com.xamoom.android.xamoomsdk.Resource.ContentBlock;
 
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,10 +44,10 @@ public class ContentBlock2ViewHolder extends RecyclerView.ViewHolder {
   private TextView mTitleTextView;
   private WebView mVideoWebView;
   private View mWebViewOverlay;
-  private String mYoutubeVideoCode;
   private YouTubeThumbnailView mYouTubeThumbnailView;
   private ImageView mVideoPlayImageView;
-  private VideoView mVideoView;
+
+  private static HashMap<String, Bitmap> mBitmapCache = new HashMap<>();
 
   public ContentBlock2ViewHolder(View itemView, Fragment fragment) {
     super(itemView);
@@ -48,72 +57,75 @@ public class ContentBlock2ViewHolder extends RecyclerView.ViewHolder {
     mWebViewOverlay = (View) itemView.findViewById(R.id.webViewOverlay);
     mYouTubeThumbnailView = (YouTubeThumbnailView) itemView.findViewById(R.id.youtube_thumbnail_view);
     mVideoPlayImageView = (ImageView) itemView.findViewById(R.id.video_play_image_view);
-    mVideoView = (VideoView) itemView.findViewById(R.id.video_view);
     WebSettings webSettings = mVideoWebView.getSettings();
     webSettings.setJavaScriptEnabled(true);
   }
 
   public void setupContentBlock(ContentBlock contentBlock) {
-    mTitleTextView.setVisibility(View.VISIBLE);
-    mYouTubeThumbnailView.setVisibility(View.VISIBLE);
-    mWebViewOverlay.setVisibility(View.VISIBLE);
-    mVideoPlayImageView.setVisibility(View.VISIBLE);
-    mVideoView.setVisibility(View.VISIBLE);
+    mTitleTextView.setVisibility(View.GONE);
+    mYouTubeThumbnailView.setVisibility(View.GONE);
+    mVideoWebView.setVisibility(View.GONE);
+    mWebViewOverlay.setVisibility(View.GONE);
+    mVideoPlayImageView.setVisibility(View.GONE);
 
-    if(contentBlock.getTitle() != null)
+    if(contentBlock.getTitle() != null) {
+      mTitleTextView.setVisibility(View.VISIBLE);
       mTitleTextView.setText(contentBlock.getTitle());
-    else {
-      mTitleTextView.setVisibility(View.GONE);
     }
 
     if(getYoutubeVideoId(contentBlock.getVideoUrl()) != null) {
+      mYouTubeThumbnailView.setVisibility(View.VISIBLE);
+      mVideoPlayImageView.setVisibility(View.VISIBLE);
       setupYoutube(contentBlock);
+    } else if (contentBlock.getVideoUrl().contains("vimeo.com/")) {
+      mWebViewOverlay.setVisibility(View.VISIBLE);
+      mVideoWebView.setVisibility(View.VISIBLE);
+      setupVimeo(contentBlock);
     } else {
+      mYouTubeThumbnailView.setVisibility(View.VISIBLE);
+      mVideoPlayImageView.setVisibility(View.VISIBLE);
       setupHTMLPlayer(contentBlock);
     }
   }
 
-  public void setupHTMLPlayer(final ContentBlock contentBlock) {
-    mYouTubeThumbnailView.setVisibility(View.GONE);
-    mVideoPlayImageView.setVisibility(View.GONE);
-
-    if (contentBlock.getVideoUrl().contains("vimeo.com/")) {
-      mVideoView.setVisibility(View.GONE);
-      String vimeoEmbed = "<iframe src=\"https://player.vimeo.com/video/"
-          + getVimeoVideoId(contentBlock.getVideoUrl()) + "?badge=0\" width=\"100%%\" " +
-          "height=\"100%%\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen " +
-          "allowfullscreen></iframe>";
-      mVideoWebView.loadData(vimeoEmbed, "text/html", "UTF-8");
-      mWebViewOverlay.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contentBlock.getVideoUrl()));
-          mFragment.getActivity().startActivity(browserIntent);
-        }
-      });
-    } else {
-      mWebViewOverlay.setVisibility(View.GONE);
-      mVideoWebView.loadUrl(contentBlock.getVideoUrl());
-      mWebViewOverlay.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-          Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(contentBlock.getVideoUrl()));
-          intent.setDataAndType(Uri.parse(contentBlock.getVideoUrl()), "video/mp4");
-          mFragment.getActivity().startActivity(intent);
-        }
-      });
-    }
+  private void setupVimeo(final ContentBlock contentBlock) {
+    String vimeoEmbed = "<iframe src=\"https://player.vimeo.com/video/"
+        + getVimeoVideoId(contentBlock.getVideoUrl()) + "?badge=0\" width=\"100%%\" " +
+        "height=\"100%%\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen " +
+        "allowfullscreen></iframe>";
+    mVideoWebView.loadData(vimeoEmbed, "text/html", "UTF-8");
+    mWebViewOverlay.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(contentBlock.getVideoUrl()));
+        mFragment.getActivity().startActivity(browserIntent);
+      }
+    });
   }
-  public void setupYoutube(ContentBlock contentBlock) {
-    mVideoWebView.setVisibility(View.GONE);
-    mWebViewOverlay.setVisibility(View.GONE);
-    mVideoView.setVisibility(View.GONE);
-    mYoutubeVideoCode = getYoutubeVideoId(contentBlock.getVideoUrl());
 
+  public void setupHTMLPlayer(final ContentBlock contentBlock) {
+    if (mBitmapCache.get(contentBlock.getVideoUrl()) != null) {
+      mYouTubeThumbnailView.setImageBitmap(mBitmapCache.get(contentBlock.getVideoUrl()));
+    } else {
+        new VideoThumbnail().execute(contentBlock.getVideoUrl());
+    }
+
+    mYouTubeThumbnailView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(contentBlock.getVideoUrl()));
+        intent.setDataAndType(Uri.parse(contentBlock.getVideoUrl()), "video/mp4");
+        mFragment.startActivity(intent);
+      }
+    });
+  }
+
+  public void setupYoutube(ContentBlock contentBlock) {
+    final String youtubeVideoId = getYoutubeVideoId(contentBlock.getVideoUrl());
     mYouTubeThumbnailView.initialize("AIzaSyBNZUh3-dj4YYY9-csOtQeHG_MpoE8x69Q", new YouTubeThumbnailView.OnInitializedListener() {
       @Override
       public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader youTubeThumbnailLoader) {
-        youTubeThumbnailLoader.setVideo(mYoutubeVideoCode);
+        youTubeThumbnailLoader.setVideo(youtubeVideoId);
         youTubeThumbnailLoader.setOnThumbnailLoadedListener(new YouTubeThumbnailLoader.OnThumbnailLoadedListener() {
           @Override
           public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
@@ -135,11 +147,10 @@ public class ContentBlock2ViewHolder extends RecyclerView.ViewHolder {
     mYouTubeThumbnailView.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Intent intent = YouTubeIntents.createPlayVideoIntent(mFragment.getActivity(), mYoutubeVideoCode);
+        Intent intent = YouTubeIntents.createPlayVideoIntent(mFragment.getActivity(), youtubeVideoId);
         mFragment.getActivity().startActivity(intent);
       }
     });
-
   }
 
   public String getYoutubeVideoId(String videoUrl) {
@@ -170,5 +181,45 @@ public class ContentBlock2ViewHolder extends RecyclerView.ViewHolder {
     }
 
     return null;
+  }
+
+  private class VideoThumbnail extends AsyncTask<String, Void, Bitmap> {
+    private String videoUrl;
+
+    @Override
+    protected Bitmap doInBackground(String... params) {
+      videoUrl = params[0];
+      return retriveVideoFrameFromVideo(params[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+      super.onPostExecute(bitmap);
+      mYouTubeThumbnailView.setImageBitmap(bitmap);
+      mBitmapCache.put(videoUrl, bitmap);
+    }
+
+    public Bitmap retriveVideoFrameFromVideo(String videoPath) {
+      Bitmap bitmap = null;
+      MediaMetadataRetriever mediaMetadataRetriever = null;
+      try {
+        mediaMetadataRetriever = new MediaMetadataRetriever();
+        if (Build.VERSION.SDK_INT >= 14) {
+          mediaMetadataRetriever.setDataSource(videoPath, new HashMap<String, String>());
+        } else {
+          mediaMetadataRetriever.setDataSource(videoPath);
+        }
+        bitmap = mediaMetadataRetriever.getFrameAtTime();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      } finally {
+        if (mediaMetadataRetriever != null) {
+          mediaMetadataRetriever.release();
+        }
+      }
+      return bitmap;
+    }
   }
 }
