ae6ecb8d1c75082fdd7f66d45d2b4613a37a755e
Raphael Seher <raphael@xamoom.com>
2016-05-19 11:12:56 +0200

    Add ContetnBlock7Adapter

--- xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/Adapters/ContentBlock7Adapter.java
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/Adapters/ContentBlock7Adapter.java
@@ -1,7 +1,47 @@
 package com.xamoom.android.xamoomcontentblocks.Adapters;
 
-/**
- * Created by raphaelseher on 18/05/16.
- */
-public class ContentBlock7Adapter {
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.util.LruCache;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xamoom.android.xamoomcontentblocks.ViewHolders.ContentBlock3ViewHolder;
+import com.xamoom.android.xamoomcontentblocks.ViewHolders.ContentBlock6ViewHolder;
+import com.xamoom.android.xamoomcontentblocks.ViewHolders.ContentBlock7ViewHolder;
+import com.xamoom.android.xamoomcontentblocks.XamoomContentFragment;
+import com.xamoom.android.xamoomsdk.EnduserApi;
+import com.xamoom.android.xamoomsdk.R;
+import com.xamoom.android.xamoomsdk.Resource.ContentBlock;
+import com.xamoom.android.xamoomsdk.Resource.Style;
+
+import java.util.List;
+
+public class ContentBlock7Adapter implements AdapterDelegate<List<ContentBlock>> {
+  private static final int BLOCK_TYPE = 7;
+
+
+  @Override
+  public boolean isForViewType(@NonNull List<ContentBlock> items, int position) {
+    ContentBlock cb = items.get(position);
+    return cb.getBlockType() == BLOCK_TYPE;
+  }
+
+  @NonNull
+  @Override
+  public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, Fragment fragment, EnduserApi enduserApi, String youtubeApiKey, LruCache bitmapCache, LruCache contentCache, boolean showContentLinks, ContentBlock3ViewHolder.OnContentBlock3ViewHolderInteractionListener onContentBlock3ViewHolderInteractionListener, XamoomContentFragment.OnXamoomContentFragmentInteractionListener onXamoomContentFragmentInteractionListener) {
+    View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.content_block_7_layout, parent, false);
+    return new ContentBlock7ViewHolder(view);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull List<ContentBlock> items, int position, @NonNull RecyclerView.ViewHolder holder, Style style) {
+    ContentBlock cb = items.get(position);
+    ContentBlock7ViewHolder newHolder = (ContentBlock7ViewHolder) holder;
+    newHolder.setStyle(style);
+    newHolder.setupContentBlock(cb);
+  }
 }
