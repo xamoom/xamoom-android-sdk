c70c0f32cb1af16ea624fdb59700f621a0f20ab3
Raphael Seher <raphael@xamoom.com>
2016-11-07 15:37:33 +0100

    Add OfflineSavingManager

---
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomsdk/Storage/OfflineSavingManager.java
@@ -0,0 +1,125 @@
+package com.xamoom.android.xamoomsdk.Storage;
+
+import android.content.Context;
+
+import com.xamoom.android.xamoomsdk.Resource.Content;
+import com.xamoom.android.xamoomsdk.Resource.Marker;
+import com.xamoom.android.xamoomsdk.Resource.Menu;
+import com.xamoom.android.xamoomsdk.Resource.Spot;
+import com.xamoom.android.xamoomsdk.Resource.System;
+import com.xamoom.android.xamoomsdk.Resource.Style;
+import com.xamoom.android.xamoomsdk.Resource.SystemSetting;
+import com.xamoom.android.xamoomsdk.Storage.Database.ContentDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.MarkerDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.MenuDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SettingDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SpotDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.StyleDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SystemDatabaseAdapter;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class OfflineSavingManager {
+  private static OfflineSavingManager mInstance;
+  private Context mContext;
+
+  private DownloadManager mDownloadManager;
+  private ContentDatabaseAdapter mContentDatabaseAdapter;
+  private SpotDatabaseAdapter mSpotDatabaseAdapter;
+  private SystemDatabaseAdapter mSystemDatabaseAdapter;
+  private StyleDatabaseAdapter mStyleDatabaseAdapter;
+  private SettingDatabaseAdapter mSettingDatabaseAdapter;
+  private MenuDatabaseAdapter mMenuDatabaseAdapter;
+
+  public static OfflineSavingManager getInstance(Context context) {
+    if (mInstance == null) {
+      mInstance = new OfflineSavingManager(context);
+    }
+    return mInstance;
+  }
+
+  private OfflineSavingManager(Context context) {
+    mContext = context;
+    mDownloadManager = new DownloadManager(FileManager.getInstance(context));
+    mContentDatabaseAdapter = ContentDatabaseAdapter.getInstance(context);
+    mSpotDatabaseAdapter = SpotDatabaseAdapter.getInstance(context);
+    mSystemDatabaseAdapter = SystemDatabaseAdapter.getInstance(context);
+    mStyleDatabaseAdapter = StyleDatabaseAdapter.getInstance(context);
+    mSettingDatabaseAdapter = SettingDatabaseAdapter.getInstance(context);
+    mMenuDatabaseAdapter = MenuDatabaseAdapter.getInstance(context);
+  }
+
+  public boolean saveContent(Content content, DownloadManager.OnDownloadManagerCompleted completion)
+      throws MalformedURLException {
+    long row = mContentDatabaseAdapter.insertOrUpdateContent(content, false, -1);
+
+    if (content.getPublicImageUrl() != null) {
+      mDownloadManager.saveFileFromUrl(new URL(content.getPublicImageUrl()),
+          false, completion);
+    }
+
+    return row != -1;
+  }
+
+  public boolean saveSpot(Spot spot, DownloadManager.OnDownloadManagerCompleted completion) throws MalformedURLException {
+    long row = mSpotDatabaseAdapter.insertOrUpdateSpot(spot);
+
+    if (spot.getPublicImageUrl() != null) {
+      mDownloadManager.saveFileFromUrl(new URL(spot.getPublicImageUrl()),
+          false, completion);
+    }
+
+    return row != -1;
+  }
+
+  public boolean saveSystem(System system) {
+    long row = mSystemDatabaseAdapter.insertOrUpdateSystem(system);
+    return row != -1;
+  }
+
+  public boolean saveStyle(Style style) {
+    long row = mStyleDatabaseAdapter.insertOrUpdateStyle(style, -1);
+    return row != -1;
+  }
+
+  public boolean saveSetting(SystemSetting setting) {
+    long row = mSettingDatabaseAdapter.insertOrUpdateSetting(setting, -1);
+    return row != -1;
+  }
+
+  public boolean saveMenu(Menu menu) {
+    long row = mMenuDatabaseAdapter.insertOrUpdate(menu, -1);
+    return row != -1;
+  }
+
+  // getter & setter
+
+  public void setContentDatabaseAdapter(ContentDatabaseAdapter contentDatabaseAdapter) {
+    mContentDatabaseAdapter = contentDatabaseAdapter;
+  }
+
+  public void setDownloadManager(DownloadManager downloadManager) {
+    mDownloadManager = downloadManager;
+  }
+
+  public void setSpotDatabaseAdapter(SpotDatabaseAdapter spotDatabaseAdapter) {
+    mSpotDatabaseAdapter = spotDatabaseAdapter;
+  }
+
+  public void setSystemDatabaseAdapter(SystemDatabaseAdapter systemDatabaseAdapter) {
+    mSystemDatabaseAdapter = systemDatabaseAdapter;
+  }
+
+  public void setStyleDatabaseAdapter(StyleDatabaseAdapter styleDatabaseAdapter) {
+    mStyleDatabaseAdapter = styleDatabaseAdapter;
+  }
+
+  public void setSettingDatabaseAdapter(SettingDatabaseAdapter settingDatabaseAdapter) {
+    mSettingDatabaseAdapter = settingDatabaseAdapter;
+  }
+
+  public void setMenuDatabaseAdapter(MenuDatabaseAdapter menuDatabaseAdapter) {
+    mMenuDatabaseAdapter = menuDatabaseAdapter;
+  }
+}
---
+++ xamoomsdk/src/test/java/com/xamoom/android/xamoomsdk/xamoomsdk/Storage/OfflineSavingManagerTest.java
@@ -0,0 +1,170 @@
+package com.xamoom.android.xamoomsdk.xamoomsdk.Storage;
+
+import com.xamoom.android.xamoomsdk.BuildConfig;
+import com.xamoom.android.xamoomsdk.Resource.Content;
+import com.xamoom.android.xamoomsdk.Resource.Menu;
+import com.xamoom.android.xamoomsdk.Resource.Spot;
+import com.xamoom.android.xamoomsdk.Resource.Style;
+import com.xamoom.android.xamoomsdk.Resource.System;
+import com.xamoom.android.xamoomsdk.Resource.SystemSetting;
+import com.xamoom.android.xamoomsdk.Storage.Database.ContentDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.MenuDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SettingDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SpotDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.StyleDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.Database.SystemDatabaseAdapter;
+import com.xamoom.android.xamoomsdk.Storage.DownloadError;
+import com.xamoom.android.xamoomsdk.Storage.DownloadManager;
+import com.xamoom.android.xamoomsdk.Storage.OfflineSavingManager;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Semaphore;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class OfflineSavingManagerTest {
+
+  private OfflineSavingManager mOfflineSavingManager;
+  private DownloadManager mMockedDownloadManager;
+  private ContentDatabaseAdapter mMockedContentDatabaseAdapter;
+  private SpotDatabaseAdapter mMockedSpotDatabaseAdapter;
+  private SystemDatabaseAdapter mMockedSystemDatabaseAdapter;
+  private StyleDatabaseAdapter mMockedStyleDatabaseAdapter;
+  private SettingDatabaseAdapter mMockedSettingDatabaseAdapter;
+  private MenuDatabaseAdapter mMockedMenuDatabaseAdapter;
+
+  @Before
+  public void setup() {
+    mOfflineSavingManager = OfflineSavingManager.getInstance(RuntimeEnvironment.application);
+    mMockedDownloadManager = Mockito.mock(DownloadManager.class);
+    mMockedContentDatabaseAdapter = Mockito.mock(ContentDatabaseAdapter.class);
+    mMockedSpotDatabaseAdapter = Mockito.mock(SpotDatabaseAdapter.class);
+    mMockedSystemDatabaseAdapter = Mockito.mock(SystemDatabaseAdapter.class);
+    mMockedStyleDatabaseAdapter = Mockito.mock(StyleDatabaseAdapter.class);
+    mMockedSettingDatabaseAdapter = Mockito.mock(SettingDatabaseAdapter.class);
+    mMockedMenuDatabaseAdapter = Mockito.mock(MenuDatabaseAdapter.class);
+
+    mOfflineSavingManager.setDownloadManager(mMockedDownloadManager);
+    mOfflineSavingManager.setContentDatabaseAdapter(mMockedContentDatabaseAdapter);
+    mOfflineSavingManager.setSpotDatabaseAdapter(mMockedSpotDatabaseAdapter);
+    mOfflineSavingManager.setSystemDatabaseAdapter(mMockedSystemDatabaseAdapter);
+    mOfflineSavingManager.setStyleDatabaseAdapter(mMockedStyleDatabaseAdapter);
+    mOfflineSavingManager.setSettingDatabaseAdapter(mMockedSettingDatabaseAdapter);
+    mOfflineSavingManager.setMenuDatabaseAdapter(mMockedMenuDatabaseAdapter);
+  }
+
+  @Test
+  public void testSaveContent() throws MalformedURLException {
+    Content content = new Content();
+    content.setId("1");
+    content.setPublicImageUrl("http://www.xamoom.com");
+
+    Mockito.stub(mMockedContentDatabaseAdapter.insertOrUpdateContent(eq(content), eq(false),
+        eq(-1))).toReturn(1L);
+
+    boolean saved = false;
+    try {
+      saved = mOfflineSavingManager.saveContent(content, null);
+    } catch (MalformedURLException e) {
+      Assert.fail();
+    }
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedContentDatabaseAdapter).insertOrUpdateContent(eq(content), eq(false), eq(-1L));
+    Mockito.verify(mMockedDownloadManager).saveFileFromUrl(
+        eq(new URL("http://www.xamoom.com")),
+        eq(false),
+        any(DownloadManager.OnDownloadManagerCompleted.class));
+  }
+
+  @Test
+  public void testSaveSpot() throws MalformedURLException {
+    Spot spot = new Spot();
+    spot.setPublicImageUrl("http://www.xamoom.com");
+
+    Mockito.stub(mMockedSpotDatabaseAdapter.insertOrUpdateSpot(eq(spot))).toReturn(1L);
+
+    boolean saved = false;
+    try {
+      saved = mOfflineSavingManager.saveSpot(spot, null);
+    } catch (MalformedURLException e) {
+      Assert.fail();
+    }
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedSpotDatabaseAdapter).insertOrUpdateSpot(eq(spot));
+    Mockito.verify(mMockedDownloadManager).saveFileFromUrl(
+        eq(new URL("http://www.xamoom.com")),
+        eq(false),
+        any(DownloadManager.OnDownloadManagerCompleted.class));
+  }
+
+  @Test
+  public void testSaveSystem() {
+    System system = new System();
+    system.setId("1");
+
+    Mockito.stub(mMockedSystemDatabaseAdapter.insertOrUpdateSystem(eq(system))).toReturn(1L);
+
+    boolean saved = mOfflineSavingManager.saveSystem(system);
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedSystemDatabaseAdapter).insertOrUpdateSystem(eq(system));
+  }
+
+  @Test
+  public void testSaveStyle() {
+    Style style = new Style();
+    style.setId("1");
+
+    Mockito.stub(mMockedStyleDatabaseAdapter.insertOrUpdateStyle(eq(style), anyLong())).toReturn(1L);
+
+    boolean saved = mOfflineSavingManager.saveStyle(style);
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedStyleDatabaseAdapter).insertOrUpdateStyle(eq(style), eq(-1L));
+  }
+
+  @Test
+  public void testSaveSetting() {
+    SystemSetting setting = new SystemSetting();
+    setting.setId("1");
+
+    Mockito.stub(mMockedSettingDatabaseAdapter.insertOrUpdateSetting(eq(setting), anyLong()))
+        .toReturn(1L);
+
+    boolean saved = mOfflineSavingManager.saveSetting(setting);
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedSettingDatabaseAdapter).insertOrUpdateSetting(eq(setting), eq(-1L));
+  }
+
+  @Test
+  public void testSaveMenu() {
+    Menu menu = new Menu();
+    menu.setId("1");
+
+    Mockito.stub(mMockedMenuDatabaseAdapter.insertOrUpdate(eq(menu), anyLong()))
+        .toReturn(1L);
+
+    boolean saved = mOfflineSavingManager.saveMenu(menu);
+
+    Assert.assertTrue(saved);
+    Mockito.verify(mMockedMenuDatabaseAdapter).insertOrUpdate(eq(menu), eq(-1L));
+  }
+}
