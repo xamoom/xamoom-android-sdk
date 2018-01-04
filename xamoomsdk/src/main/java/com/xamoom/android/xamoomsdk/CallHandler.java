6fba9ab2327c401b2b90d7975923f6040c3c10de
Raphael Seher <raphael@xamoom.com>
2018-01-04 15:51:25 +0100

    Add better errors to Callhandler & refactor

--- xamoomsdk/src/main/java/com/xamoom/android/xamoomsdk/CallHandler.java
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomsdk/CallHandler.java
@@ -8,6 +8,10 @@
 
 package com.xamoom.android.xamoomsdk;
 
+import android.support.annotation.NonNull;
+
+import org.json.JSONException;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,37 +31,29 @@ import retrofit2.Response;
 public class CallHandler <T extends Resource> {
   public static final String ERROR_CODE_CALL_FAILURE = "10000";
   public static final String ERROR_CODE_PARSING_JSON_FAILED = "10001";
-  public static final String ERROR_CODE_RESPONSE_IS_NULL = "10002";
+  public static final String ERROR_CODE_CONNECTION_FAILURE = "10003";
+  public static final String ERROR_CODE_REQUEST_OR_RESPONSE_FAILURE = "10003";
   public static final String ERROR_MESSAGE_RESPONSE_IS_NULL = "Parsed response is null. " +
       "Check if data really exists.";
 
 
   private Morpheus morpheus;
 
-  public CallHandler(Morpheus morpheus) {
+  public CallHandler(@NonNull Morpheus morpheus) {
     this.morpheus = morpheus;
   }
 
-  public void enqueCall(Call<ResponseBody> call, final APICallback<T, List<Error>> callback) {
-    if (call == null) {
-      throw new NullPointerException("call is null");
-    }
+  public void enqueCall(@NonNull Call<ResponseBody> call, final APICallback<T, List<Error>> callback) {
+    checkForNull(call);
 
     call.enqueue(new Callback<ResponseBody>() {
       @Override
       public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-        String json = getJsonFromResponse(response);
-
-        JsonApiObject jsonApiObject = null;
+        JsonApiObject jsonApiObject;
         try {
-          jsonApiObject = morpheus.parse(json);
+          jsonApiObject = parseResponse(response);
         } catch (Exception e) {
-          callback.error(createError(e.getMessage(), ERROR_CODE_PARSING_JSON_FAILED));
-          return;
-        }
-
-        if (jsonApiObject == null) {
-          callback.error(createError(ERROR_MESSAGE_RESPONSE_IS_NULL, ERROR_CODE_RESPONSE_IS_NULL));
+          callback.error(createError(e));
           return;
         }
 
@@ -71,40 +67,31 @@ public class CallHandler <T extends Resource> {
 
       @Override
       public void onFailure(Call<ResponseBody> call, Throwable t) {
-        callback.error(createError(t.getMessage(), ERROR_CODE_CALL_FAILURE));
+        callback.error(createError(t));
       }
     });
   }
 
-  public void enqueListCall(Call<ResponseBody> call, final APIListCallback<List<T>,
+  public void enqueListCall(@NonNull Call<ResponseBody> call, final APIListCallback<List<T>,
       List<Error>> callback) {
-    if (call == null) {
-      throw new NullPointerException("call is null");
-    }
+    checkForNull(call);
 
     call.enqueue(new Callback<ResponseBody>() {
       @Override
       public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-        String json = getJsonFromResponse(response);
-
-        JsonApiObject jsonApiObject = null;
+        JsonApiObject jsonApiObject;
         try {
-          jsonApiObject = morpheus.parse(json);
+          jsonApiObject = parseResponse(response);
         } catch (Exception e) {
-          callback.error(createError(e.getMessage(), ERROR_CODE_PARSING_JSON_FAILED));
-          return;
-        }
-
-        if (jsonApiObject == null) {
-          callback.error(createError(ERROR_MESSAGE_RESPONSE_IS_NULL, ERROR_CODE_RESPONSE_IS_NULL));
+          callback.error(createError(e));
           return;
         }
 
         if (jsonApiObject.getResources() != null) {
-          List<T> contents = (List<T>) jsonApiObject.getResources();
+          List<?> contents = jsonApiObject.getResources();
           String cursor = jsonApiObject.getMeta().get("cursor").toString();
           boolean hasMore = (boolean) jsonApiObject.getMeta().get("has-more");
-          callback.finished(contents, cursor, hasMore);
+          callback.finished((List<T>) contents, cursor, hasMore);
         } else if (jsonApiObject.getErrors() != null && jsonApiObject.getErrors().size() > 0) {
           callback.error(jsonApiObject.getErrors());
         }
@@ -112,9 +99,15 @@ public class CallHandler <T extends Resource> {
 
       @Override
       public void onFailure(Call<ResponseBody> call, Throwable t) {
-        callback.error(createError(t.getMessage(), ERROR_CODE_CALL_FAILURE));
+        callback.error(createError(t));
       }
     });
+  }
+
+  private void checkForNull(Call call) {
+    if (call == null) {
+      throw new NullPointerException("Call object is null");
+    }
   }
 
   private String getJsonFromResponse(Response<ResponseBody> response) {
@@ -139,11 +132,39 @@ public class CallHandler <T extends Resource> {
     return json;
   }
 
-  private List<Error> createError(String message, String code) {
+  private JsonApiObject parseResponse(Response response) throws Exception {
+    String json = getJsonFromResponse(response);
+
+    JsonApiObject jsonApiObject;
+    try {
+      jsonApiObject = morpheus.parse(json);
+    } catch (Exception e) {
+      throw e;
+    }
+
+    if (jsonApiObject == null) {
+      throw new NullPointerException(ERROR_MESSAGE_RESPONSE_IS_NULL);
+    }
+
+    return jsonApiObject;
+  }
+
+  private List<Error> createError(Throwable t) {
+    String code;
+    if (t instanceof IOException) {
+      code = ERROR_CODE_CONNECTION_FAILURE;
+    } else if (t instanceof RuntimeException) {
+      code =  ERROR_CODE_REQUEST_OR_RESPONSE_FAILURE;
+    } else if (t instanceof JSONException) {
+      code = ERROR_CODE_PARSING_JSON_FAILED;
+    } else {
+      code = ERROR_CODE_CALL_FAILURE;
+    }
+
     Error error = new Error();
-    error.setDetail(message);
+    error.setDetail(t.getMessage());
     error.setCode(code);
-    List<Error> errors = new ArrayList<Error>();
+    List<Error> errors = new ArrayList<>();
     errors.add(error);
     return errors;
   }
