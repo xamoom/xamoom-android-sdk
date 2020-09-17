586d76a57a632720ba4886aa0206d6880c172c0e
G0yter <goyter012@gmail.com>
2020-09-17 10:01:10 +0300

    npe bug fixed

--- xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/ViewHolders/ContentBlock1ViewHolder.java
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomcontentblocks/ViewHolders/ContentBlock1ViewHolder.java
@@ -151,16 +151,18 @@ public class ContentBlock1ViewHolder extends RecyclerView.ViewHolder {
           AudioPlayerService.MSG_SET_URL);
       msg.replyTo = messenger;
       Bundle bundle = new Bundle();
-      bundle.putString("URL", contentBlock.getFileId());
-      bundle.putString("POS", contentBlock.getId());
-      bundle.putString("TITLE", contentBlock.getTitle());
-      bundle.putString("ARTIST", contentBlock.getArtists());
+      if(contentBlock != null) {
+        bundle.putString("URL", contentBlock.getFileId());
+        bundle.putString("POS", contentBlock.getId());
+        bundle.putString("TITLE", contentBlock.getTitle());
+        bundle.putString("ARTIST", contentBlock.getArtists());
+      }
       msg.setData(bundle);
 
       try {
         mService.send(msg);
       } catch (RemoteException e) {
-        e.printStackTrace();
+        System.out.println("ContentBlock1ViewHolder:163 " + e.getMessage());
       }
     }
 
