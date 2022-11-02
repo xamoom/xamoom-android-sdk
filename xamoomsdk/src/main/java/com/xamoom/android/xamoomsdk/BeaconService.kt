3e7ede6671426a5335fac40bfedbca749c0ea15a
Raphael Seher <raphael.seher@gmail.com>
2022-11-02 16:07:52 +0100

    Add stop action for foreground service

--- xamoomsdk/src/main/java/com/xamoom/android/xamoomsdk/BeaconService.kt
+++ xamoomsdk/src/main/java/com/xamoom/android/xamoomsdk/BeaconService.kt
@@ -1,17 +1,29 @@
 package com.xamoom.android.xamoomsdk
 
 import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.util.Log
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.lifecycle.Observer
 import org.altbeacon.beacon.*
 import timber.log.Timber
-import kotlin.properties.Delegates
 
-class BeaconService(context: Context, majorId: Int) {
+class BeaconServiceBroadcastReceiver() : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == BeaconService.NOTIFICATION_ACTION_STOP_SERVICE) {
+            BeaconService.sharedInstance?.disableForegroundService()
+        }
+    }
+}
+
+class BeaconService(
+    context: Context, majorId: Int,
+    @StringRes private val notificationTitle: Int = R.string.beacon_service_notification_title,
+    @DrawableRes private val notificationIcon: Int = R.drawable.ic_near_me
+) {
 
     val beaconViewModel: BeaconViewModel = BeaconViewModel()
 
@@ -28,8 +40,7 @@ class BeaconService(context: Context, majorId: Int) {
         if (state == MonitorNotifier.INSIDE) {
             Timber.d("Background: Beacons detected")
             if (automaticRanging) startRanging()
-        }
-        else {
+        } else {
             Timber.d("Background: No beacons detected")
             if (automaticRanging) stopRanging()
             beaconViewModel.clearBeacons()
@@ -44,6 +55,14 @@ class BeaconService(context: Context, majorId: Int) {
         }
     }
 
+    // kotlin does not recognize my default parameters
+    constructor(context: Application, beaconMajor: Int) : this(
+        context,
+        beaconMajor,
+        R.string.beacon_service_notification_title,
+        R.drawable.ic_near_me
+    ) {}
+
     init {
         beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BEACON_LAYOUT))
 
@@ -51,6 +70,8 @@ class BeaconService(context: Context, majorId: Int) {
 
         beaconManager.getRegionViewModel(region).regionState.observeForever(backgroundObserver)
         beaconManager.getRegionViewModel(region).rangedBeacons.observeForever(rangingObserver)
+
+        sharedInstance = this
     }
 
     /**
@@ -69,29 +90,52 @@ class BeaconService(context: Context, majorId: Int) {
         stopRanging()
 
         val builder = Notification.Builder(context)
-        builder.setSmallIcon(R.drawable.ic_android)
-        builder.setContentTitle("Scanning for Beacons")
-        val intent = Intent(context, clazz)
+        builder.setSmallIcon(notificationIcon)
+        builder.setContentTitle(context.getString(notificationTitle))
+
+        // opening the app when clicking on the notification
+        val intent = context.packageManager.getLaunchIntentForPackage(context.packageName);
         val pendingIntent = PendingIntent.getActivity(
             context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
         )
         builder.setContentIntent(pendingIntent)
 
+        // Action to stop the service
+        val disableService = Intent(NOTIFICATION_ACTION_STOP_SERVICE)
+        disableService.setClass(context, BeaconServiceBroadcastReceiver::class.java)
+        val disableServicePendingIntent: PendingIntent =
+            PendingIntent.getBroadcast(context, 0, disableService, 0)
+        builder.addAction(
+            R.drawable.ic_stop_24,
+            context.getString(R.string.beacon_service_stop_action),
+            disableServicePendingIntent
+        )
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                "beacon-ref-notification-id",
-                "My Notification Name",
+                NOTIFICATION_CHANNEL_ID,
+                context.getString(R.string.beacon_service_notification_name),
                 NotificationManager.IMPORTANCE_LOW
             )
-            channel.description = "My Notification Channel Description"
+            channel.description =
+                context.getString(R.string.beacon_service_notification_description)
+
+            context.startActivity(intent);
+
             val notificationManager = context.getSystemService(
-                Context.NOTIFICATION_SERVICE) as NotificationManager
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
             notificationManager.createNotificationChannel(channel)
             builder.setChannelId(channel.id)
         }
 
         foregroundNotification = builder.build()
-        foregroundNotification?.let { beaconManager.enableForegroundServiceScanning(it, 456) }
+        foregroundNotification?.let {
+            beaconManager.enableForegroundServiceScanning(
+                it,
+                NOTIFICATION_ID
+            )
+        }
         beaconManager.setEnableScheduledScanJobs(false)
     }
 
@@ -125,6 +169,13 @@ class BeaconService(context: Context, majorId: Int) {
 
     companion object {
         const val BEACON_LAYOUT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
-        val XAMOOM_BEACON_IDENTIFIER: Identifier = Identifier.parse("de2b94ae-ed98-11e4-3432-78616d6f6f6d")
+        const val NOTIFICATION_CHANNEL_ID: String = "foreground-beacon-service-channel-id";
+        const val NOTIFICATION_ID: Int = 1001;
+        const val NOTIFICATION_ACTION_STOP_SERVICE: String =
+            "com.xamoom.android.xamoomsdk.NOTIFICATION_ACTION_STOP_SERVICE";
+        val XAMOOM_BEACON_IDENTIFIER: Identifier =
+            Identifier.parse("de2b94ae-ed98-11e4-3432-78616d6f6f6d")
+
+        var sharedInstance: BeaconService? = null
     }
 }
